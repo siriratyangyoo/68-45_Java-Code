e6008cb4e3450e662665110d87f86ee318a935a5
siriratyangyoo <siriratyungyoo1234@gmail.com>
2025-10-01 10:58:18 +0700

    Delete java/Loop/java directory

--- java/Loop/java/W02_01_Area_Square.java
+++
@@ -1,7 +0,0 @@
-public class W02_01_Area_Square {
-    public static void main(String[] args) {
-        int side = 4;
-        int area = side * side;
-        System.out.println("พื้นที่สี่เหลี่ยมจัตุรัสที่มีด้าน = "+side+" คือ "+area);
-    }
-}
--- java/Loop/java/W02_01_SumNumber.java
+++
@@ -1,8 +0,0 @@
-public class W02_01_SumNumber {
-    public static void main(String[] args) {
-        int x = 9999;
-        int y = 1;
-        int sum = x+y;
-        System.out.println("ผลรวมของ " + x + "+" + y + "=" + sum);
-    }
-}
